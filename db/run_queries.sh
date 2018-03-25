13003a289c77c167f996e3b0ba6f40b91cc6c0a4
kimmyfek <kimmyfek@gmail.com>
2018-03-25 11:10:09 -0600

    fix sql insert script

--- db/run_queries.sh
+++ db/run_queries.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-#mysql -h localhost -u root -ppassword -e "show databases"
-docker run -it --link passbook_db:mysql -v insert.sql:/tmp --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" DenverPassbook < /tmp/insert.sql'
+docker run -it --link passbook_db:mysql -v $PWD:/tmp --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" DenverPassbook < /tmp/create.sql'
+docker run -it --link passbook_db:mysql -v $PWD:/tmp --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" DenverPassbook < /tmp/business_insert.sql'
+docker run -it --link passbook_db:mysql -v $PWD:/tmp --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" DenverPassbook < /tmp/location_insert.sql'
+docker run -it --link passbook_db:mysql -v $PWD:/tmp --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" DenverPassbook < /tmp/coup_insert.sql'
+docker run -it --link passbook_db:mysql -v $PWD:/tmp --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" DenverPassbook < /tmp/hours_insert.sql'
