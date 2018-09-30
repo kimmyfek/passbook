a940bbbd08fcb991368167e2cf43a86ebe27c366
kimmyfek <kimmyfek@gmail.com>
2018-09-29 23:55:57 -0600

    add side menu bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,20 +2,20 @@
   <div class="top-nav">
   <b-navbar toggleable="md" type="dark" variant="info">
 
-    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-
     <b-navbar-brand href="#">Denver Passbook</b-navbar-brand>
+    <sidebarmenu></sidebarmenu>
 
-    <b-collapse is-nav id="nav_collapse">
-
-    </b-collapse>
   </b-navbar>
   </div>
 </template>
 
 <script>
+import SidebarMenu from '@/components/SidebarMenu'
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  components: {
+    'sidebarmenu': SidebarMenu
+  }
 }
 </script>
 
---
+++ src/components/SidebarMenu.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="menu">
+  <Slide right noOverlay>
+    <h2> Filter </h2>
+    <b-form-group label="Neighborhood(s)">
+      <b-form-checkbox-group
+        stacked
+        v-model="selectedNeighborhoods"
+        :options="neighborhoods"
+        @input="checkboxToggle">
+      </b-form-checkbox-group>
+    </b-form-group>
+  </Slide>
+  </div>
+</template>
+
+<script>
+import { Slide } from 'vue-burger-menu'
+export default {
+  name: 'SidebarMenu',
+  components: {
+    Slide
+  },
+  methods: {
+    checkboxToggle () {
+      console.log('clicked')
+    }
+  },
+  data () {
+    return {
+      selectedNeighborhoods: [],
+      // TODO: move to backend API
+      neighborhoods: [
+        {text: 'Ballpark', value: 'Ballpark'},
+        {text: 'Cap Hill', value: 'Cap Hill'},
+        {text: 'Cherry Creek', value: 'Cherry Creek'},
+        {text: 'City Park', value: 'City Park'},
+        {text: 'Downtown', value: 'Downtown'},
+        {text: 'DTC', value: 'DTC'},
+        {text: 'East Denver', value: 'East Denver'},
+        {text: 'Glendale', value: 'Glendale'},
+        {text: 'Golden Triangle', value: 'Golden Triangle'},
+        {text: 'Govs Park', value: 'Govs Park'},
+        {text: 'Highlands', value: 'Highlands'},
+        {text: 'Hilltop', value: 'Hilltop'},
+        {text: 'Lakewood', value: 'Lakewood'},
+        {text: 'Littleton', value: 'Littleton'},
+        {text: 'Lodo', value: 'Lodo'},
+        {text: 'North Denver', value: 'North Denver'},
+        {text: 'Platt Park', value: 'Platt Park'},
+        {text: 'RiNo', value: 'RiNo'},
+        {text: 'Sloans Lake', value: 'Sloans Lake'},
+        {text: 'South Broadway', value: 'South Broadway'},
+        {text: 'South Denver', value: 'South Denver'},
+        {text: 'Stapleton', value: 'Stapleton'},
+        {text: 'Virginia Village', value: 'Virginia Village'},
+        {text: 'Wash Park', value: 'Wash Park'},
+        {text: 'West Denver', value: 'West Denver'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .bm-burger-button {
+    top: 10px;
+  }
+  .bm-burger-bars {
+      background-color: white;
+  }
+  .bm-menu {
+    text-align: left;
+  }
+</style>
